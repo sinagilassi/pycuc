d02d2c4164a36311ef647b36215bb2cec55f18fa
sina.gilassi <sina.gilassi@gmail.com>
2024-09-18 02:39:35 -0400

    add setup.py

---
+++ setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+APP_NAME = 'Python Custom Unit Converter'
+VERSION = '1.0.0'
+DESCRIPTION = 'PyCUC: A lightweight Python package for creating custom unit conversions.'
+LONG_DESCRIPTION = "PyCUC: A lightweight Python package for creating custom unit conversions. Easily define and convert between units with simplicity and flexibility"
+
+# Setting up
+setup(
+    name=APP_NAME,
+    version=VERSION,
+    author="Sina Gilassi",
+    author_email="<sina.gilassi@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(exclude=['tests', '*.tests', '*.tests.*']),
+    license='MIT',
+    install_requires=['pandas', 'numpy'],
+    keywords=['python', 'chemical engineering', 'custom unit conversion',
+              'PyCUC'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Education",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ],
+    python_requires='>=3.6',
+)
