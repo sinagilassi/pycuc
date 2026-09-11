813f9c3405adfd210a00aa187de6c540597450fc
Sina Gilassi <51185548+sinagilassi@users.noreply.github.com>
2026-09-11 15:46:52 -0400

    Expose unit algebra public API

---
+++ pycuc/algebra/__init__.py
@@ -0,0 +1,23 @@
+"""Symbolic unit algebra for PyCUC."""
+
+from .compatibility import are_compatible, require_compatible
+from .expression import HasUnit, infer_unit, infer_unit_string
+from .parser import parse_unit, unit
+from .simplify import divide_units, multiply_units, power_unit, simplify_unit
+from .unit import ExponentInput, UnitExpr
+
+__all__ = [
+    "ExponentInput",
+    "HasUnit",
+    "UnitExpr",
+    "are_compatible",
+    "divide_units",
+    "infer_unit",
+    "infer_unit_string",
+    "multiply_units",
+    "parse_unit",
+    "power_unit",
+    "require_compatible",
+    "simplify_unit",
+    "unit",
+]
