8d713e21f3297eaf3d3a9b49b3f69f9b0c38ac08
Sina Gilassi <51185548+sinagilassi@users.noreply.github.com>
2026-09-11 21:40:48 -0400

    Export base-unit expansion and UnitStyle

--- pycuc/algebra/__init__.py
+++ pycuc/algebra/__init__.py
@@ -1,8 +1,9 @@
 """Symbolic unit algebra for PyCUC."""
 
+from .base import expand_to_base_units
 from .compatibility import are_compatible, require_compatible
 from .derived import reduce_derived_unit
-from .expression import HasUnit, infer_unit, infer_unit_string
+from .expression import HasUnit, UnitStyle, infer_unit, infer_unit_string
 from .parser import parse_unit
 from .simplify import divide_units, multiply_units, power_unit, simplify_unit
 from .unit import ExponentInput, UnitExpr
@@ -11,8 +12,10 @@ __all__ = [
     "ExponentInput",
     "HasUnit",
     "UnitExpr",
+    "UnitStyle",
     "are_compatible",
     "divide_units",
+    "expand_to_base_units",
     "infer_unit",
     "infer_unit_string",
     "multiply_units",
